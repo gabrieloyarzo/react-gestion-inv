9e1cffdacda32735ac1468d3df728c2ff9990aa3
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-04-28 15:14:13 -0400

    button in each table row

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "./table.css";
 
 const baseURL = "https://gestion-inv-api.onrender.com/api/producto";
@@ -13,6 +14,16 @@ const data = await response.json();
 console.log(data);
 
 function Table() {
+  const [hoveredRow, setHoveredRow] = useState(null);
+
+  const handleMouseEnter = (id) => {
+    setHoveredRow(id);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredRow(null);
+  };
+
   return (
     <div className="tabla-datos">
       <table>
@@ -28,12 +39,17 @@ function Table() {
         <tbody>
           {data &&
             data.map((item) => (
-              <tr key={item.id}>
+              <tr
+                key={item.id}
+                onMouseEnter={() => handleMouseEnter(item.id)}
+                onMouseLeave={handleMouseLeave}
+              >
                 <td>{item.id}</td>
                 <td>{item.nombre}</td>
                 <td>{item.categoria}</td>
                 <td>{item.stock}</td>
                 <td>{item.precio}</td>
+                <td>{hoveredRow === item.id && <button>Button</button>}</td>
               </tr>
             ))}
         </tbody>
