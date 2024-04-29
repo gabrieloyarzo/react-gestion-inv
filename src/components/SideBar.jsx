475de3e9dafe89750ecdfb2f2625458d14f3f227
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-04-28 21:14:36 -0400

    Sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,19 +7,19 @@ function Sidebar() {
         <nav>
           <div id="contenedor-button">
             <a id="proveedores" href="#">
-              <img src="/panel_principal/icons/cliente.png" />
+              <img src="./images/proveedores.png" />
               <h1>Proveedores</h1>
             </a>
             <a id="ventas" href="#">
-              <img src="/panel_principal/icons/ventas.png" />
+              <img src="./images/ventas.png" />
               <h1>Ventas</h1>
             </a>
             <a id="productos" href="#">
-              <img src="/panel_principal/icons/productos.png" />
+              <img src="./images/productos.png" />
               <h1>Productos</h1>
             </a>
             <a id="pedidos" href="#">
-              <img src="/panel_principal/icons/pedidos.png" />
+              <img src="./images/pedidos.png" />
               <h1>Pedidos</h1>
             </a>
           </div>
