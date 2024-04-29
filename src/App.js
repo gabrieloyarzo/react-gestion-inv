c15b9f9207d7a246681993eb4f608683012a3690
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-04-28 21:09:27 -0400

    asd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Banner from "./components/Banner";
-import Sidebar from "./components/Sidebar";
 import Button from "./components/Button";
 import Table from "./components/Table";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
