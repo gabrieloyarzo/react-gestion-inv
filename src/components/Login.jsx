71c76fd9ade0e7e690fd930c48f6ba478fa6710d
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-06 20:34:30 -0400

    change with prettier Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,36 @@
 /*LOGIN ver. 0.2*/
 
-import './login.css';
+import "./login.css";
 import logo_plus from "../images/logo_plus.png";
 
 const Login = () => {
-    return (
-        <div className="container">
-          <div className="sombra-azul"></div>
-          <div className="izquierda">
-            {/*<h1>ERP</h1>*/}
-            <img src={logo_plus}/>
+  return (
+    <div className="container">
+      <div className="sombra-azul"></div>
+      <div className="izquierda">
+        {/*<h1>ERP</h1>*/}
+        <img src={logo_plus} />
+      </div>
+      <div className="derecha">
+        <h1>¡BIENVENIDO!</h1>
+        <h2>Accede a tu cuenta</h2>
+        <form action="#" method="post">
+          <input type="text" placeholder="Usuario" name="usuario" required />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            name="contraseña"
+            required
+          />
+          <div className="checkbox-container">
+            <input type="checkbox" id="recordar" name="Recordar" value="0" />
+            <label htmlFor="recordar">Recordar usuario</label>
           </div>
-          <div className="derecha">
-            <h1>¡BIENVENIDO!</h1>
-            <h2>Accede a tu cuenta</h2>
-            <form action="#" method="post">
-              <input type="text" placeholder="Usuario" name="usuario" required />
-              <input type="password" placeholder="Contraseña" name="contraseña" required />
-              <div className="checkbox-container">
-                <input type="checkbox" id="recordar" name="Recordar" value="0" />
-                <label htmlFor="recordar">Recordar usuario</label>
-              </div>
-              <button type="submit">Ingresar</button>
-            </form>
-          </div>
-        </div>
-      );
-    }
-    
-    export default Login;
+          <button type="submit">Ingresar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
