c454183dd7839182c2c0898cb937be6f1290b4ec
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-09 17:54:55 -0400

    add apiService.js -> ApiOrders

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -70,3 +70,74 @@ export const ApiProducts = {
     }
   },
 };
+
+export const ApiOrders = {
+  async getAllOrders() {
+    try {
+      const response = await fetch(`${API_URL}/pedido`);
+      if (!response.ok) {
+        throw new Error("Error al obtener pedidos");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error al obtener pedidos:", error);
+      throw error;
+    }
+  },
+
+  async createOrder(orderData) {
+    try {
+      const response = await fetch(`${API_URL}/order`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+      });
+      if (!response.ok) {
+        throw new Error("Error al crear pedido");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error al crear pedido:", error);
+      throw error;
+    }
+  },
+
+  async updateOrder(orderId, updatedOrderData) {
+    try {
+      const response = await fetch(`${API_URL}/pedido/${orderId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedOrderData),
+      });
+      if (!response.ok) {
+        throw new Error("Error al actualizar pedido");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error al actualizar pedido:", error);
+      throw error;
+    }
+  },
+
+  async deleteOrder(orderId) {
+    try {
+      const response = await fetch(`${API_URL}/pedido/${orderId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Error al eliminar pedido");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error al eliminar pedido:", error);
+      throw error;
+    }
+  },
+}
